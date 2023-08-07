4b0f84ec0ef5769d91fbe3f2b65a1cb0c66ddbc7
Marcus Steele <marcuspaulo1905@gmail.com>
2023-08-07 15:19:00 -0300

    Realizando atividades do 71 até 75

--- atividade-61-65.py
+++ atividade-61-65.py
@@ -1,124 +1,98 @@
-""" atividade 56
-Desenvolva um programa que leia o nome, idade e sexo de 4 pessoas. No final
-do programa, mostre:
+""" Atividade 61 e 62
+Leia o primeiro Termo e a razão de uma PA, Mostre os 10
+primeiros termos usando uma estrutura While
 
-A média de idade do grupo
-Qual é o nome do homem mais velho
-Quantas mulheres tem menos de 20 anos
+Realize um upgrade perguntando se o usuário quer adicionar
+mais alguns termos e caso ele não queira, encerre o programa
+e antes disso mostre os termos da PA
 """
-pessoas = []
-idades = []
-    # Criação das pessoas
-for pessoa in range(0, 5):
-    pessoa = []
-    nome = input('Digite o seu nome: ')
-    idade = int(input('Digite a sua idade: '))
-    sexo = input('Digite o seu sexo (M/F): ')
-    pessoa.append(nome)
-    pessoa.append(idade)
-    pessoa.append(sexo)
-    pessoas.append(pessoa)
-    idades.append(pessoa[1])
-print(pessoas)
+primeiro_termo = int(input('Digite o primeiro termo da PA: '))
+razao = int(input('Digite a razão da PA: '))
 
-    # Soma das idades 
-print(f'A média de idade é {int(sum(idades))/ len(pessoas)}')
-maior_idade = max(idades)
-for p in pessoas:
-    # Pessoa com maior idade
-    if maior_idade == p[1]:
-        print(f'Está é o nome da pessoa com a maior idade {p[0]}')
-print('Mulheres: ')
-for p in pessoas:
-    # Verificando o genero 
-    
-    if p[2] == 'F':
-        print(p[0])
+termos = []
+def cria_pa(a1, r):
+    condicao = True
+    an = a1
+    contador = 0
+    while contador <= 10:
+        an += r
+        print(an)
+        contador += 1
+        termos.append(an)
+    while condicao:
+        opcao = input('Deseja adicionar mais termos?').lower()
+        if opcao == 's' or opcao == 'sim':
+          resposta = input(f'Este é o ultimo termo {termos[-1]} e está é a razão {r}, logo o próximo termo seria {termos[-1] + r}, deseja adiciona-lo?').lower()
+          if resposta == 's' or opcao == 'sim':
+              termos.append(termos[-1] + r)
+        else:
+            print(termos)
+            print('Obrigado por usar o nosso sistema!')
+            break
+cria_pa(primeiro_termo, razao)
 
-""" atividade 57
-Faça um programa que leia o sexo de uma pessoa, mas so aceite os valores "M" ou "F", caso
-esteja errado, peça a digitação novamente até que seja inserido um valor correto
+
+
+""" Atividade 63 
+Escreva um programa que leia um número inteiro qualquer e mostre na tela
+os 6 primeiros números de uma sequencia fibonacci
 """
-
-condicao = True
-while condicao:
-    sexo = input('Digite o seu sexo[M/F]: ').strip().upper()
-    if sexo == 'M' or sexo == 'F':
-        print('Muito bem, resposta correta.')
-        condicao = False
-
-
-""" atividade 58
-Faça um desafio onde o computador vai gerar um numero entre 0 e 10, so que agora o jogador
-vai tentar adivinhar até acertar, mostrando no final quantos palpites foram necessários
-para vencer
-"""
-import random
-contador = 0
-numero_aleatorio = random.randint(1, 10)
-c = True
-while c:
-    palpite = int(input('Digite o seu palpite: '))
-    contador += 1
-    if palpite == numero_aleatorio:
-        print('Parabéns você acertou!!')
-        print(f'Você realizou {contador} tentativas')
-        c = False
-    else:
-        print('Infelizmente você errou o palpite, tente novamente.')
-        print(f'Você realizou {contador} tentativas')
+entrada = int(input('Digite um número: '))
+qtd_termos = int(input('Digite a quantidade de termos desejados: '))
+def sequencia_fibonacci(n):
+    sequencia = []
+    sequencia.append(n)
+    sequencia.append(n)
+    sequencia.append(n + n)
+    while True:
+        novo_num = round((sequencia[-1]) + (sequencia[-2]))
+        sequencia.append(novo_num)
+        if len(sequencia) == qtd_termos:
+            break 
+    print(sequencia)
+sequencia_fibonacci(entrada)
 
 
+""" Atividade 64 
+Crie um programa que leia vários números inteiros pelo teclado.
+ O programa só vai parar quando o usuário digitar o valor 999, que a
+ condição de parada. No final, mostre quantos números foram digitados
+ e qual foi a soma entre eles
+ """
 
-""" atividade 59
-Crie um programa que leia dois valores e mostre um menu como ao lado na tela e seu programa
-deverá realizar a operação solicitada em cada caso
-[1]somar
-[2]multiplicar
-[3]maior
-[4]novos números
-[5]sair
+def le_num():
+    lista_de_num = []
+    while True:
+        numero = int(input('Digite um número: '))
+        if numero ==  999:
+            print(f'O somatório é: {sum(lista_de_num)}')
+            print(f'Você digitou {len(lista_de_num)} números,', lista_de_num)
+            break
+        lista_de_num.append(numero)
+le_num()
+
+
+""" Atividade 65 
+Crie um programa que leia 5 números inteiros pelo teclado. No final da execução
+, mostre a média entre todos os valores e qual foi o maior e o menor valor lido
+O programa deve perguntar ao usuário se ele quer ou não continuar a digitar valores
 """
 
-from time import sleep
-delay = 1
-c2 = True
-
-valor1 = int(input('Digite um valor: '))
-valor2 = int(input('Digite um segundo valor: '))
-while c2:
-
-    sleep(delay)
-    print("[1] Somar")
-    print("[2] Multiplicar")
-    print("[3] Maior")
-    print("[4] Novos Números")
-    print("[5] Sair")
-    escolha = int(input('Digite a opção desejada: '))
-    match escolha:
-        case 1:
-            print(f'A soma entre os dois valores digitados é {valor1 + valor2}')
-        case 2:
-            print(f'A multiplicação entre os dois valores digitados é {valor1 * valor2}')
-        case 3:
-
-            valores = [valor1, valor2]
-            print(f'Este é o maior valor entre os dois {max(valores)}')
-        case 4:
-            valor1 = int(input('Digite um novo valor: '))
-            valor2 = int(input('Digite um segundo novo valor: '))
-            print('Valores alterados')
-        case 5:
-            print('Volte sempre!!')
-            c2 = False
-
-
-""" atividade 60
-Faça um programa que leia qualquer número e mostre o seu fatorial
-"""
-resultado = 1
-fatorial = int(input('Digite um número para saber o seu valor fatorial: '))
-while fatorial >= 1:
-    resultado *= fatorial
-    fatorial -= 1
-print(f'O valor fatorial de {fatorial} é {resultado}') 
+def gera_num():
+    lista_de_numeros = []
+    while True:
+        numero = int(input('Digite um número: '))
+        if len(lista_de_numeros) == 5 or len(lista_de_numeros) == 10:
+            print(f'O maior número digitado é: {max(lista_de_numeros)}')
+            print(f'O menor número digitado é: {min(lista_de_numeros)}')
+            print(f'Você digitou {len(lista_de_numeros)} números,', lista_de_numeros)
+            nova_resposta = input('Deseja continuar? ').lower()
+            if nova_resposta == 'não' or nova_resposta == 'nao':
+                break
+        lista_de_numeros.append(numero)
+        if len(lista_de_numeros) == 15:
+            print('Programa encerrou')
+            print(f'Esses foram os números digitados {lista_de_numeros}')
+            break
+            
+gera_num()
